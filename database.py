747e8450d342c2f1826388219042b20eaf0e98f2
ippolkovnikovas-hash <ippolkovnikovas@gmail.com>
2026-07-10 16:07:57 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from typing import AsyncGenerator
 
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import declarative_base
 
 DATABASE_URL = "sqlite+aiosqlite:///./cookbook.db"
